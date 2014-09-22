8a284787a381b5953775afa321d9b59f7ee7e44b
Mike <rodrigues.mi@husky.neu.edu>
2014-09-22 15:33:32 -0400

    Fixing io redirection dup2 calls per today's class

--- shell_exec.c
+++ shell_exec.c
@@ -21,37 +21,6 @@ int execute_cmd(status *s) {
     return 0;
   }
 
-  // Store copies of the original file descriptors so we can switch back to them afterwards
-  int stdin_fd_alias  = dup(STDIN_FILENO);
-  int stdout_fd_alias = dup(STDOUT_FILENO);
-  int stderr_fd_alias = dup(STDERR_FILENO);
-
-  int temp_fd; // temporary file descriptor value for the swapping of file descriptors
-  if (s->fds[STDIN_FILENO]) { // if we have an input redirection
-    temp_fd = open(s->fds[STDIN_FILENO], O_RDONLY); // open the file for reading as input
-    if (temp_fd == -1) {
-      printf("Error: Unable to open redirection file.\n"); // print required msg
-      return 1;
-    }
-    dup2(temp_fd, STDIN_FILENO); // swap the opened stdin redirect file with stdin
-  }
-  if (s->fds[STDOUT_FILENO]) { // if we have an output redirection
-    temp_fd = open(s->fds[STDOUT_FILENO], O_WRONLY | O_TRUNC | O_CREAT, 0777); // open file, create/overwrite contents, permissions: 777
-    if (temp_fd == -1) {
-      printf("Error: Unable to open redirection file.\n"); // print required msg
-      return 1;
-    }
-    dup2(temp_fd, STDOUT_FILENO); // swap the opened stdout redirect file with stdout
-  }
-  if (s->fds[STDERR_FILENO]) { // if we have a stderr output redirection
-    temp_fd = open(s->fds[STDERR_FILENO], O_WRONLY | O_TRUNC | O_CREAT, 0777); // open file, create/overwrite contents, permissions: 777
-    if (temp_fd == -1) {
-      printf("Error: Unable to open redirection file.\n"); // print required msg
-      return 1;
-    }
-    dup2(temp_fd, STDERR_FILENO); // swap the opened stderr redirect file with stderr
-  }
-
   pid_t pid = fork(); // fork and capture the PID
 
   if (pid < 0) {  // if pid < 0, then fork failed.
@@ -60,7 +29,43 @@ int execute_cmd(status *s) {
   }
 
   if (pid == 0) { // child process code
+    // Store copies of the original file descriptors so we can switch back to them afterwards
+    int stdin_fd_alias  = dup(STDIN_FILENO);
+    int stdout_fd_alias = dup(STDOUT_FILENO);
+    int stderr_fd_alias = dup(STDERR_FILENO);
+
+    int temp_fd; // temporary file descriptor value for the swapping of file descriptors
+    int error = 0; // do we have any errors opening redirection files?
+    if (s->fds[STDIN_FILENO]) { // if we have an input redirection
+      temp_fd = open(s->fds[STDIN_FILENO], O_RDONLY); // open the file for reading as input
+      if (temp_fd == -1) {
+        printf("Error: Unable to open redirection file.\n"); // print required msg
+        error++;
+      }
+      dup2(temp_fd, STDIN_FILENO); // swap the opened stdin redirect file with stdin
+    }
+    if (s->fds[STDOUT_FILENO]) { // if we have an output redirection
+      temp_fd = open(s->fds[STDOUT_FILENO], O_WRONLY | O_TRUNC | O_CREAT, 0777); // open file, create/overwrite contents, permissions: 777
+      if (temp_fd == -1) {
+        printf("Error: Unable to open redirection file.\n"); // print required msg
+        error++;
+      }
+      dup2(temp_fd, STDOUT_FILENO); // swap the opened stdout redirect file with stdout
+    }
+    if (s->fds[STDERR_FILENO]) { // if we have a stderr output redirection
+      temp_fd = open(s->fds[STDERR_FILENO], O_WRONLY | O_TRUNC | O_CREAT, 0777); // open file, create/overwrite contents, permissions: 777
+      if (temp_fd == -1) {
+        printf("Error: Unable to open redirection file.\n"); // print required msg
+        error++;
+      }
+      dup2(temp_fd, STDERR_FILENO); // swap the opened stderr redirect file with stderr
+    }
+    if (error > 0) { exit(1); } // if we hit an error redirecting io
     if (execvp(exec_argv[0], exec_argv) == -1) { // if there was an error with execvp
+      // swap child's file descriptors back to parent's before printing errors
+      dup2(stdin_fd_alias,  STDIN_FILENO);  
+      dup2(stdout_fd_alias, STDOUT_FILENO);
+      dup2(stderr_fd_alias, STDERR_FILENO);
       if (errno == EACCES) { // if an error is thrown trying execvp
         printf("Error: Permission denied.\n");
       } else {
@@ -69,12 +74,6 @@ int execute_cmd(status *s) {
     }
     exit(1); // have child process exit
   } else { // parent process code
-    // swap parent's file descriptors back to normal before returning
-    // note: this was the only way I could figure out how to do it.. trying to swap the fds for just the child caused things to print multiple times.
-    dup2(stdin_fd_alias,  STDIN_FILENO);  
-    dup2(stdout_fd_alias, STDOUT_FILENO);
-    dup2(stderr_fd_alias, STDERR_FILENO);
-
     if (!s->bg_proc) { // if this is not a background process then wait for it to exit before returning
       waitpid(pid, NULL, 0);
     }
